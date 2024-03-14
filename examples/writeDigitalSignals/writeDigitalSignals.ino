b24af95ce86b79187b58d3861c074aaff4670977
Nick Lamprianidis <info@nlamprian.me>
2024-03-14 21:28:29 +0100

    Reformat code

--- examples/writeDigitalSignals/writeDigitalSignals.ino
+++ examples/writeDigitalSignals/writeDigitalSignals.ino
@@ -2,7 +2,7 @@
  * This example demonstrates how to write digital signals
  * It assumes there are LEDs+resistors with the positive lead of the LEDs
  * connected to the 16 channels of the 74HC4067 mux/demux, respectively
- * 
+ *
  * For more about the interface of the library go to
  * https://github.com/nlamprian/MUX74HC4067
  */
@@ -14,24 +14,20 @@
 // 2nd-5th arguments are the Arduino PINs to which the S0-S3 pins connect
 MUX74HC4067 mux(7, 8, 9, 10, 11);
 
-void setup()
-{
-	// Configures how the SIG pin will be interfaced
-	// e.g. The SIG pin connects to PIN 3 on the Arduino,
-	//      and PIN 3 is a Digital Output
-	mux.signalPin(3, OUTPUT, DIGITAL);
+void setup() {
+  // Configures how the SIG pin will be interfaced
+  // e.g. The SIG pin connects to PIN 3 on the Arduino,
+  //      and PIN 3 is a Digital Output
+  mux.signalPin(3, OUTPUT, DIGITAL);
 }
 
 // Writes to the 16 channels a HIGH value, one after the other
-void loop()
-{
-	for (byte i = 0; i < 16; ++i)
-	{
-		// Connects to channel i and writes HIGH
-		mux.write(i, HIGH);
-		delay(25);
-	}
-	
-	mux.disable();  // Disconnects the SIG pin from any channel
-	delay(800);
+void loop() {
+  for (byte i = 0; i < 16; ++i) {
+    mux.write(i, HIGH); // Connects to channel i and writes HIGH
+    delay(25);
+  }
+
+  mux.disable(); // Disconnects the SIG pin from any channel
+  delay(800);
 }
--- examples/writePWMSignals/writePWMSignals.ino
+++ examples/writePWMSignals/writePWMSignals.ino
@@ -2,7 +2,7 @@
  * This example demonstrates how to write PWM signals
  * It assumes there are LEDs+resistors with the positive lead of the LEDs
  * connected to the 16 channels of the 74HC4067 mux/demux, respectively
- * 
+ *
  * For more about the interface of the library go to
  * https://github.com/nlamprian/MUX74HC4067
  */
@@ -14,25 +14,21 @@
 // 2nd-5th arguments are the Arduino PINs to which the S0-S3 pins connect
 MUX74HC4067 mux(7, 8, 9, 10, 11);
 
-void setup()
-{
-	// Configures how the SIG pin will be interfaced
-	// e.g. The SIG pin connects to PIN 3 on the Arduino,
-	//      and PIN 3 is a Analog (PWM) Output
-	mux.signalPin(3, OUTPUT, ANALOG);
+void setup() {
+  // Configures how the SIG pin will be interfaced
+  // e.g. The SIG pin connects to PIN 3 on the Arduino,
+  //      and PIN 3 is a Analog (PWM) Output
+  mux.signalPin(3, OUTPUT, ANALOG);
 }
 
 // Writes to the 16 channels a PWM output, one after the other
 // At each next channel, the duty cycle of the PWM increases
-void loop()
-{
-	for (byte i = 0; i < 16; ++i)
-	{
-		// Connects to channel i and outputs a PWM signal
-		mux.write(i, i * 16 + 1);
-		delay(200);
-	}
-	
-	mux.disable();  // Disconnects the SIG pin from any channel
-	delay(1000);
+void loop() {
+  for (byte i = 0; i < 16; ++i) {
+    mux.write(i, i * 16 + 1); // Connects to channel i and outputs a PWM signal
+    delay(200);
+  }
+
+  mux.disable(); // Disconnects the SIG pin from any channel
+  delay(1000);
 }
